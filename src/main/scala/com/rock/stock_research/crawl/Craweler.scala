6f615a1d0185cddbf449d8655c750547e7575b83
rhuang <rock.huang@morningstar.com>
2014-08-21 15:55:02 +0800

    add yui

---
+++ src/main/scala/com/rock/stock_research/crawl/Craweler.scala
@@ -0,0 +1,29 @@
+package com.rock.stock_research.crawl
+import scala.collection.mutable.ArrayBuffer
+import com.rock.stock_research.http.Http
+
+object Craweler extends App {
+
+  val indexURLS = Array("http://app.finance.ifeng.com/hq/list.php?type=stock_a&class=ha", "http://app.finance.ifeng.com/hq/list.php?type=stock_a&class=sa", "http://app.finance.ifeng.com/hq/list.php?type=stock_a&class=gem")
+
+  val stockRegex = "http://finance.ifeng.com/app/hq/stock/([a-z0-9]+)/index.shtml".r
+
+  val stocks = ArrayBuffer.empty[String]
+
+  for (url <- indexURLS) {
+
+    val html = new String(Http.get(url).data)
+
+    stockRegex.findAllIn(html).matchData.foreach {
+
+      m => stocks += m.group(1)
+
+    }
+
+  }
+
+  println(stocks)
+
+  println(stocks.length + "," + stocks.toSet.size)
+
+}
--- src/main/scala/com/rock/stock_research/launch/JettyLauncher.scala
+++ src/main/scala/com/rock/stock_research/launch/JettyLauncher.scala
@@ -1,14 +1,14 @@
 package com.rock.stock_research.launch
 
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.servlet.{ DefaultServlet, ServletContextHandler }
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
 object JettyLauncher { // this is my entry object as specified in sbt project definition
   def main(args: Array[String]) {
     println("server start...")
-    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+    val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8089
 
     val server = new Server(port)
     val context = new WebAppContext()
