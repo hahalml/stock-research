ca83df29e521c555f8223b6266d3fd7e2aec2b62
rhuang <rock.huang@morningstar.com>
2014-08-22 18:46:15 +0800

    add StockStatistics

---
+++ src/main/scala/com/rock/stock_research/statistics/StockStatistics.scala
@@ -0,0 +1,25 @@
+package com.rock.stock_research.statistics
+
+case class StockStatistics() {
+  var stCode: String = null
+  var name: String = null
+  var startDate: String = null
+  var endDate: String = null
+  var min: Double = Double.NaN
+  var max: Double = Double.NaN
+  var avg: Double = Double.NaN
+  var curr: Double = Double.NaN
+  var totalDealNum: Int = Int.MinValue
+  var totalDealPrice: Double = Double.NaN
+  var incPercent: String = null
+  var lastIncPercent: String = null
+
+  def toMap = {
+    Map("stCode" -> stCode, "name" -> name, "startDate" -> startDate, "endDate" -> endDate, "min" -> min, "max" -> max, "avg" -> avg, "curr" -> curr,
+      "totalDealNum" -> totalDealNum, "totalDealPrice" -> totalDealPrice, "incPercent" -> incPercent, "lastIncPercent" -> lastIncPercent)
+  }
+}
+
+object TestStat extends App {
+
+}
