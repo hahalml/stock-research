b32e021fa05494fd0e888da4c868d532b8c40e60
rhuang <rock.huang@morningstar.com>
2014-08-28 17:11:47 +0800

    add IndexAction

---
+++ src/main/scala/com/rock/stock_research/action/IndexAction.scala
@@ -0,0 +1,11 @@
+package com.rock.stock_research.action
+
+import org.scalatra.FlashMapSupport
+import org.scalatra.scalate.ScalateSupport
+import org.scalatra.ScalatraServlet
+
+class IndexAction extends ScalatraServlet with FlashMapSupport with ScalateSupport {
+  get("/index-infos-by-day") {
+
+  }
+}
