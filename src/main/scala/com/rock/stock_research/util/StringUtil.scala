6cd202a0f5268e56fa4a5537b88e010aef71bc1c
rhuang <rock.huang@morningstar.com>
2014-12-26 14:30:40 +0800

    add StringUtil

---
+++ src/main/scala/com/rock/stock_research/util/StringUtil.scala
@@ -0,0 +1,12 @@
+package com.rock.stock_research.util
+
+object StringUtil extends App{
+    val asciiLetter  = ('0' to '9') ++ ('a' to 'z') ++ ('A' to 'Z')
+    def getFirstLetter(c:Char) = {
+      c
+    }
+    //http://blog.csdn.net/chunlongyuan/article/details/8514147
+    //http://www.oschina.net/code/snippet_203635_11850
+    
+    println(asciiLetter )
+}
